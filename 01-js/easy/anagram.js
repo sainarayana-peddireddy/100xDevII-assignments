604bf7cf6e69967d02fe9c2b611eeeafad24ae67
Sai Narayana Peddireddy Kurakula <sainarayana.k@infosys.com>
2023-12-29 23:41:31 +0530

    anagram code

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,37 @@
 */
 
 function isAnagram(str1, str2) {
+  const aa = [];
 
+  if(str1.length !==  str2.length)
+  return false
+  
+  
+  str1 = str1.toLowerCase()
+  str2 = str2.toLowerCase()
+  
+  let c = 0;
+  while(c < 256){
+    aa.push(0);
+    c++;
+  }
+
+  
+  for(let i = 0; i < str1.length; i+=1 ){
+    aa[str1.charCodeAt(i)] = 1;
+  }
+
+  for(let i = 0; i < str2.length; i+=1 ){
+    aa[str2.charCodeAt(i)] = 0;
+  }
+
+  c = 0
+  while(c < 256){
+    if(aa[c] == 1)
+      return false
+    c++;
+  }
+  return true;
 }
 
 module.exports = isAnagram;
